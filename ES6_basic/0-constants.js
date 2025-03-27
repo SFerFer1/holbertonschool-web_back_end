8fb5ad9fe7ab1a5062f781c103a6f7607cab8dbe
SFerFer1 <10048@holbertonstudents.com>
2025-03-27 09:49:55 -0300

    a

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,9 +1,15 @@
-function taskFirst() {
-    const a = 5;
-
-}
-
-function taskNext() {
-    let b = 10;
-
-}
+export function taskFirst() {
+    const task = "I prefer const when I can.";
+    return task;
+  }
+  
+  export function getLast() {
+    return " is okay";
+  }
+  
+  export function taskNext() {
+    let combination = "But sometimes let";
+    combination += getLast();
+  
+    return combination;
+  }
