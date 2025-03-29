304791f1f4c35883c86c02a714acdb51094390db
root <root@LAPTOP-4NQDD1B1.>
2025-03-29 20:00:45 -0300

    q

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-    const stringToConcat = typeof string === 'string' ? string : '';
-    return [...array1, ...array2, ...stringToConcat];
+  const stringToConcat = typeof string === 'string' ? string : '';
+  return [...array1, ...array2, ...stringToConcat];
 }
