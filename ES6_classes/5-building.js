de3692849e3cebd8e6b928fc9e3e01828bfbb7ef
root <root@LAPTOP-4NQDD1B1.>
2025-03-30 12:37:54 -0300

    just tests

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error(
-        'Class extending Building must override evacuationWarningMessage' // Changed to single quotes
+        'Class extending Building must override evacuationWarningMessage'
       );
     }
     this._sqft = sqft;
