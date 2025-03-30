f67f799e5d32fddf1075946ea79be0cc46e08187
root <root@LAPTOP-4NQDD1B1.>
2025-03-30 19:15:29 -0300

    just tests

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -29,15 +29,15 @@ export class HolbertonClass {
     }
   
     get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Fixed `this` instead of `self`
+      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
     }
   }
   
-  
+
   const class2019 = new HolbertonClass(2019, "San Francisco");
   const class2020 = new HolbertonClass(2020, "San Francisco");
   
-  
+
   const student1 = new StudentHolberton("Guillaume", "Salva", class2020);
   const student2 = new StudentHolberton("John", "Doe", class2020);
   const student3 = new StudentHolberton("Albert", "Clinton", class2019);
