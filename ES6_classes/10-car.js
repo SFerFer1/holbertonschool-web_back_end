a75e4b98a13d694122b60a96e4172113f8bf3faf
root <root@LAPTOP-4NQDD1B1.>
2025-03-30 19:07:26 -0300

    just tests

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,19 @@
+const cloneSymbol = Symbol("clone");
+
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  [cloneSymbol]() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
+
+export default Car;
