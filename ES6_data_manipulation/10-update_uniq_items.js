fb3055c57cd64557877e86deab33b82af5256952
root <root@LAPTOP-4NQDD1B1.>
2025-04-06 19:08:00 -0300

    a

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+    map.forEach((value, item) => {
+    if (value === 1) {
+      map.set(item, 100);
+    }
+    });
+};
+
+export default updateUniqueItems;
