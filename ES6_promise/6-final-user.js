9472f599e0887db03c07ae69a0992733811b20ca
SFerFer1 <10048@holbertonstudents.com>
2025-04-10 17:11:18 -0300

    S

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ])
+    .then((results) => results);
+}
