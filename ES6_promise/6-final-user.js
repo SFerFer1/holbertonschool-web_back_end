e4e25f93875b39f4df9e348561bb392fc67ca9d6
SFerFer1 <10048@holbertonstudents.com>
2025-04-10 17:41:01 -0300

    S

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,7 +5,13 @@ function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      }
+      return { status: 'rejected', reason: result.reason };
+    }));
 }
 
 export default handleProfileSignup;
