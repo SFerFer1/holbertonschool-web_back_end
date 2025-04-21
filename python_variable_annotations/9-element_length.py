2fac14a098835fe3c099ee60035a4920ed343aa5
SFerFer1 <10048@holbertonstudents.com>
2025-04-21 15:27:48 -0300

    as

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,14 +3,14 @@
 """
 This module provides a simple string operation for concatenation.
 """
-from typing import Iterable, Tuple, List
+
 
 
 from typing import Iterable, Sequence, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     This module provides a simple string operation for concatenation.
     """
     return [(i, len(i)) for i in lst]
-
