be18c040a8290dd3e04a0baa1b572cc1705700cc
SFerFer1 <10048@holbertonstudents.com>
2025-04-21 14:55:13 -0300

    as

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -7,6 +7,6 @@ This module provides mathematical utility functions using the math library.
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """Return the largest integer less than or equal to the given float number."""
-    return float(math.floor(n))
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a simple string operation for concatenation.
+"""
+
+
+def to_str(n: float) -> str:
+    return n
