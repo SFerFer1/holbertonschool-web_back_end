a66d38aa9f1026e1dd052db2dbde0db10357ae3f
SFerFer1 <10048@holbertonstudents.com>
2025-04-22 10:19:59 -0300

    as

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-
 """
 This module provides a simple string operation for concatenation.
 """
-import importlib.util
-
-spec = importlib.util.spec_from_file_location("wait_random_module", "./0-basic_async_syntax.py")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
 import asyncio
+from typing import List
+import importlib
+wait_random = importlib.import_module('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int):
-    tasks = [module.wait_random(max_delay) for _ in range(n)]
 
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This module provides a simple string operation for concatenation.
+    """
     delays = []
+    tasks = [wait_random(max_delay) for a in range(n)]
 
-    async for task in asyncio.as_completed(tasks):
+    for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
 
-    return delays 
+    return delays
