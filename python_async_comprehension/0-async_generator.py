00ccfa8fd1b4121518fee2307a8be7edc2dc1e0c
SFerFer1 <10048@holbertonstudents.com>
2025-04-28 09:11:58 -0300

    as

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple string operation for concatenation.
+"""
+import random
+import asyncio
+async def async_generator():
+    """
+    This module provides a simple string operation for concatenation.
+    """
+    for a in range(10):
+        await asyncio.sleep(1)
+    yield random.randint(0, 10)
