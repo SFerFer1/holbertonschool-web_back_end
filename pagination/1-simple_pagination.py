0620ff1d6901495ceba18891c8a18a7c55cb85a8
SFerFer1 <10048@holbertonstudents.com>
2025-04-28 12:01:21 -0300

    as

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,18 +2,52 @@
 """
 This module provides a simple string operation for concatenation.
 """
+import csv
+from math import ceil
+from typing import List, Tuple
+import os
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     This module provides a simple string operation for concatenation.
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size
     return (start_index, end_index)
 
-def get_page(page=1, page_size=10):
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
 
-    return index_range(page, page_size)
+class Server:
+    """
+    This module provides a simple string operation for concatenation.
+    """
+    DATA_FILE = os.path.join(
+        os.path.dirname(__file__),
+    )
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        This module provides a simple string operation for concatenation.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        This module provides a simple string operation for concatenation.
+        """
+        
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        return dataset[start_index:end_index]
