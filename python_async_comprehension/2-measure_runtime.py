7c6fbc763db45e55fbf0edffa32a9e8531dd3f5f
SFerFer1 <10048@holbertonstudents.com>
2025-04-28 13:47:26 -0300

    as

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,22 +1,65 @@
 #!/usr/bin/env python3
 """
-This module provides a simple string operation for concatenation.
+Deletion-resilient hypermedia pagination
 """
-import time
-import asyncio
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+import csv
+from typing import List, Dict
+import os
 
 
-async def measure_runtime() -> float:
-    """
-    This module provides a simple string operation for concatenation.
-    """
-    start = time.time()
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = os.path.join(
+        os.path.dirname(__file__), "Popular_Baby_Names.csv"
+    )
 
-    tasks = [async_comprehension() for a in range(4)]
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
 
-    await asyncio.gather(*tasks)
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
 
-    end = time.time()
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
-    return end - start
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        This module provides a simple string operation for concatenation.
+        """
+        assert index is not None and index >= 0
+
+        indexed_dataset = self.indexed_dataset()
+        assert index < len(indexed_dataset)
+
+        data = []
+        current_index = index
+        for _ in range(page_size):
+            while current_index not in indexed_dataset:
+                current_index += 1
+                if current_index >= len(indexed_dataset):
+                    break
+            if current_index >= len(indexed_dataset):
+                break
+            data.append(indexed_dataset[current_index])
+            current_index += 1
+
+        next_index = current_index
+        return {
+            "index": index,
+            "data": data,
+            "page_size": page_size,
+            "next_index": next_index,
+        }
