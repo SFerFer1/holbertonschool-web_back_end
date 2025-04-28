d1d9061fa93151ba0aaecfb86d5714405af2ad50
SFerFer1 <10048@holbertonstudents.com>
2025-04-28 11:34:16 -0300

    as

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple string operation for concatenation.
+"""
+
+
+def index_range(page, page_size):
+    """
+    This module provides a simple string operation for concatenation.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+def get_page(page=1, page_size=10):
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
+
+    index_range(page, page_size)
