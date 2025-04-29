542ec56088e91eb47c2364e00b420b09a208518d
SFerFer1 <10048@holbertonstudents.com>
2025-04-29 11:57:16 -0300

    as

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,4 +5,7 @@ This module provides a simple string operation for concatenation.
 
 
 def schools_by_topic(mongo_collection, topic):
+    """
+    This module provides a simple string operation for concatenation.
+    """
     return list(mongo_collection.find({"topics": topic}))
