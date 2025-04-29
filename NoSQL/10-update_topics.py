739a71beaf6b62930ef3f5ff39020d1692c9a7c0
SFerFer1 <10048@holbertonstudents.com>
2025-04-29 11:51:54 -0300

    as

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,7 +4,8 @@ This module provides a simple string operation for concatenation.
 """
 
 def update_topics(mongo_collection, name, topics):
-    mongo_collection.update_many(
-        { "name": name },
-        { "$set": { "topics": topics } }
-    )
+    """
+    This module provides a simple string operation for concatenation.
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+
