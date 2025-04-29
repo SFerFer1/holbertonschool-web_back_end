24c5d266ff8946742719d4a17c5c2fe54e3aefea
SFerFer1 <10048@holbertonstudents.com>
2025-04-29 13:48:50 -0300

    as

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,21 +3,21 @@
 This module provides a simple string operation for concatenation.
 """
 from pymongo import MongoClient
+if __name__ == "__main__":
+    client = MongoClient()
 
-client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-db = client.logs
-collection = db.nginx
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-total_logs = collection.count_documents({})
-print(f"{total_logs} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {collection.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection.count_documents({'method': 'DELETE'})}")
 
-print("Methods:")
-print(f"\tmethod GET: {collection.count_documents({'method': 'GET'})}")
-print(f"\tmethod POST: {collection.count_documents({'method': 'POST'})}")
-print(f"\tmethod PUT: {collection.count_documents({'method': 'PUT'})}")
-print(f"\tmethod PATCH: {collection.count_documents({'method': 'PATCH'})}")
-print(f"\tmethod DELETE: {collection.count_documents({'method': 'DELETE'})}")
-
-status_count = collection.count_documents({"method": "GET", "path": "/status"})
-print(f"method GET /status: {status_count}")
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"method GET /status: {status_count}")
