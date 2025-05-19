c5d219bb6a7c9a06fc4aca8bbc438f810c550f86
SFerFer1 <10048@holbertonstudents.com>
2025-05-19 12:34:20 -0300

    as

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const DB = process.argv[2]; // El nombre del archivo CSV
+
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.write('This is the list of our students\n');
+    try {
+      const report = await countStudents(DB);
+      res.end(report);
+    } catch (err) {
+      res.end(err.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
