220c03152650ca24d9882d6aa6fd3e2695254ab2
SFerFer1 <10048@holbertonstudents.com>
2025-05-21 13:01:47 -0300

    as

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,16 @@
 const readline = require('readline');
 
-
 const interfaz = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-
 console.log('Welcome to Holberton School, what is your name?');
 
 interfaz.on('line', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
+  console.log(`Your name is: ${input.trim()}\r`);
   interfaz.close();
 });
-
 
 interfaz.on('close', () => {
   console.log('This important software is now closing');
