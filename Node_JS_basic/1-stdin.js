11d522732c3691d4f2f371fb4e1e83e02ddda4cf
SFerFer1 <10048@holbertonstudents.com>
2025-05-21 13:13:05 -0300

    as

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
 const readline = require('readline');
 
-const interfaz = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+console.log("Welcome to Holberton School, what is your name?");
 
-interfaz.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input.trim()}\n`);
-  interfaz.close();
+rl.on('line', (input) => {
+    console.log(`Your name is: ${input.trim()}`);
+    rl.close();
 });
 
-interfaz.on('close', () => {
-  console.log('\nThis important software is now closing');
+rl.on('close', () => {
+    console.log("This important software is now closing");
 });
