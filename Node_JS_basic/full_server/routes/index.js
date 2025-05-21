5e9d55cb11d46c99bdee906dfce7e82ac4f57adb
SFerFer1 <10048@holbertonstudents.com>
2025-05-21 11:25:01 -0300

    as

---
+++ Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+
+router.get('/', AppController.home);
+
+
+router.get('/students', StudentsController.getAllStudents);
+
+
+router.get('/students/:major', StudentsController.getStudentsByMajor);
+
+module.exports = router;
